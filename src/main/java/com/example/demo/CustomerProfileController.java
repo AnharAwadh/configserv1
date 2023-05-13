c0b1578de15b7fa5d6b9acea0ad9d8154151e006
AnharAwadh <anhar9617@gmail.com>
2023-05-13 23:26:32 +0300

    update

--- src/main/java/com/example/demo/CustomerProfileController.java
+++ src/main/java/com/example/demo/CustomerProfileController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-
+@RefreshScope
 
 public class CustomerProfileController {
     @Value("${test.test}")
