405bb9d8ee4185fdc395413a419dad50cf49e0d5
Taleb <tmbksa@hotmail.com>
2023-05-14 00:35:32 +0300

    fix

--- src/main/java/com/example/demo/Demo9Application.java
+++ src/main/java/com/example/demo/Demo9Application.java
@@ -3,15 +3,16 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class Demo9Application {
 
 
-    @Value("${test.test}")
-    Integer value;
+
 
 
 
